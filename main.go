1563ad6e9e2ac4f89efbd893a2ec9d7e26be5abe
Anass Benabou <124387088+BENABOU48@users.noreply.github.com>
2024-11-07 18:12:30 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,3 +9,16 @@ func main(){
   }
     z01.PrintRune('\n')
 }
+
+
+package main
+
+import "github.com/01-edu/z01"
+
+func main(){
+  for i:='1' ; i<= '9'  ;i++{
+    
+  z01.PrintRune(i)
+  }
+    z01.PrintRune('\n')
+}
